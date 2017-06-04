cc95f90f6fa865ce27d5ab3c7f48080c0847ac6e
salomonelli <sara.steiert@outlook.com>
2017-06-04 14:21:14 +0200

    ADD template

---
+++ src/components/resume-template.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="resume" id="template">
+  <h1>This is a template</h1>
+</div>
+</template>
+
+<script>
+import {
+  PERSON
+} from '../person';
+
+import Vue from 'vue';
+export default Vue.component('resume-template', {
+  name: 'resume-template',
+  data () {
+    return {
+      person: PERSON
+    };
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+</style>

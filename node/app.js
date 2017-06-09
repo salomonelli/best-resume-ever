44e7a2e14e65ba585b40e445059ac513f3a48765
salomonelli <sara.steiert@outlook.com>
2017-06-09 22:26:23 +0200

    ADD export script

--- node/app.js
+++ node/app.js
@@ -1,26 +1,55 @@
+/* eslint-disable */
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
+const Rx = require('rxjs/Rx');
+const http = require('http');
 
-/**
- * converts resumes to pdf
- * @return {Promise}
- */
-const convert = () => {
+const fetchResponse = () => {
+  return new Promise((res, rej) => {
+    try {
+      const req = http.request('http://localhost:8080/#/', response => res(response.statusCode));
+      req.on('error', (err) => rej(err));
+      req.end();
+    } catch (err) {
+      rej(err);
+    }
+  });
+};
+
+const waitForServerReachable = () => {
+  return Rx.Observable
+    .interval(1000)
+    .mergeMap(async() => {
+      try {
+        const statusCode = await fetchResponse();
+        if (statusCode === 200) return true;
+      } catch (err) {}
+      return false;
+    })
+    .filter(ok => !!ok);
+};
+
+const timedOut = timeout => {
+  return new Promise(res => {
+    setTimeout(res, timeout);
+  });
+};
+
+const convert = async() => {
+  await waitForServerReachable().first().toPromise();
+  console.log('Connected to server ...');
+  console.log('Exporting ...');
   try {
     const directories = getResumesFromDirectories();
     const scripts = directories.map(resume => electroshotScript(resume.path));
-    execBash(scripts.join(' && '));
+    await execBash(scripts.join(' && '));
   } catch (err) {
     throw new Error(err);
   }
+  console.log('Finished exports.');
 };
 
-/**
- * generates electroshot command for screenshoting resume
- * @param  {string} resume resume name in URL
- * @return {string}        electroshot command
- */
 const electroshotScript = resume => {
   const dir = path.join(__dirname, '../pdf');
   return 'electroshot localhost:8080/#/resume/' + resume +
@@ -28,39 +57,26 @@ const electroshotScript = resume => {
     ' --filename "' + resume + '.pdf" --pdf-background';
 };
 
-/**
- * gets resumes names and paths from directories
- * @return {Object[]} array with resumes object {path: '', name: ''}
- */
 const getResumesFromDirectories = () => {
   const directories = getDirectories();
   return directories
-  .filter(dir => dir.includes('resume-') && dir !== 'resume-XX')
-  .map(dir => {
-    let name = dir.replace('resume-', '');
-    let fileName = dir.replace('.vue', '');
-    return {
-      path: fileName.replace('resume-', ''),
-      name: name.replace('-', ' ')
-    };
-  });
+    .filter(dir => dir.includes('resume-') && dir !== 'resume-XX')
+    .map(dir => {
+      let name = dir.replace('resume-', '');
+      let fileName = dir.replace('.vue', '');
+      return {
+        path: fileName.replace('resume-', ''),
+        name: name.replace('-', ' ')
+      };
+    });
 };
 
-/**
- * gets directories starting with 'resume-'
- * @return {[]}
- */
 const getDirectories = () => {
   const srcpath = path.join(__dirname, '../src/components');
   return fs.readdirSync(srcpath)
     .filter(file => file.includes('resume-'));
 };
 
-/**
- * executes command
- * @param  {string} script e.g. 'echo "Hello World"'
- * @return {Promise}
- */
 const execBash = script => {
   return new Promise((resolve, reject) => {
     exec(script,

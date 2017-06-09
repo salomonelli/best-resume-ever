f66f3d9f01fa99cb80275ae1e25636a03c2db089
salomonelli <sara.steiert@outlook.com>
2017-06-09 23:07:14 +0200

    UPDATE size of logo

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -17,33 +17,33 @@
     <div class="preview">
       <router-link v-bind:to="'/resume/material-blue'">
         <div class="preview-wrapper">
-        <img src="../assets/preview/resume-material-blue.png" />
-        <span>material-blue</span>
-      </div>
+          <img src="../assets/preview/resume-material-blue.png" />
+          <span>material-blue</span>
+        </div>
       </router-link>
     </div>
     <div class="preview">
       <router-link v-bind:to="'/resume/left-right'">
         <div class="preview-wrapper">
-        <img src="../assets/preview/resume-left-right.png" />
-        <span>left-right</span>
-      </div>
+          <img src="../assets/preview/resume-left-right.png" />
+          <span>left-right</span>
+        </div>
       </router-link>
     </div>
     <div class="preview">
       <router-link v-bind:to="'/resume/oblique'">
         <div class="preview-wrapper">
-        <img src="../assets/preview/resume-oblique.png" />
-        <span>oblique</span>
-      </div>
+          <img src="../assets/preview/resume-oblique.png" />
+          <span>oblique</span>
+        </div>
       </router-link>
     </div>
     <div class="preview">
       <router-link v-bind:to="'/resume/side-bar'">
         <div class="preview-wrapper">
-        <img src="../assets/preview/resume-side-bar.png" />
-        <span>side-bar</span>
-      </div>
+          <img src="../assets/preview/resume-side-bar.png" />
+          <span>side-bar</span>
+        </div>
       </router-link>
     </div>
   </div>
@@ -62,12 +62,13 @@ export default Vue.component('resume', {
   font-family: 'Roboto' !important;
 }
 
-.logo{
+.logo {
   text-align: center;
 }
 
 .logo img {
-  height: 200px;
+  height: 50px;
+  margin-top: 40px;
 }
 
 .title {
@@ -75,51 +76,52 @@ export default Vue.component('resume', {
   text-align: center;
   width: 100%;
   color: black;
-    font-weight: 300;
-    font-size: 30px;
-    line-height: 110%;
-    margin: 1.78rem 0 1.424rem 0;
-    margin-top: 0px;
-margin-bottom: 40px;
+  font-weight: 300;
+  font-size: 30px;
+  line-height: 110%;
+  margin: 1.78rem 0 1.424rem 0;
+  margin-top: 0px;
+  margin-bottom: 40px;
 }
 
-.previews{
+.previews {
   width: 90%;
   margin-right: auto;
   margin-left: auto;
 }
 
-.preview{
+.preview {
   width: 180px;
-    float: left;
-    margin-left: 1.5%;
-    margin-right: 1.5%;
-    margin-bottom: 1.5%;
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
-    height: 252px;
-    overflow: hidden;
+  float: left;
+  margin-left: 1.5%;
+  margin-right: 1.5%;
+  margin-bottom: 1.5%;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+  height: 252px;
+  overflow: hidden;
 }
+
 .preview-wrapper {
   position: relative;
   background: white;
 }
 
-.preview img{
+.preview img {
   width: 100%;
   opacity: 0.5;
   filter: blur(1px);
 }
 
-.preview span{
+.preview span {
   position: absolute;
-    max-width: 100%;
-    font-size: 24px;
-    font-weight: 300;
-    color: rgba(0, 0, 0, 0.75);
-    width: 100%;
-    text-align: center;
-    display: inline-block;
-    top: 50%;
-    transform: translateY(-50%);
+  max-width: 100%;
+  font-size: 24px;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 0.75);
+  width: 100%;
+  text-align: center;
+  display: inline-block;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>

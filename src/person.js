345ea30a36cbb8d440b919da79289f4240feb33b
Paul Sajna <paulsajna@gmail.com>
2017-09-10 00:00:01 -0700

    remove personal bits

--- src/person.js
+++ src/person.js
@@ -1,95 +1,106 @@
 /* eslint-disable */
 export const PERSON = {
   name: {
-    first: 'Paul',
-    last: 'Sajna',
+    first: 'John',
+    last: 'Doe',
   },
   position: 'Software Developer',
   birth: {
-    year: 1996,
-    location: 'Kelowna'
+    year: 1990,
+    location: 'New York'
   },
   experience: [{
-      company: 'Boomers Computers',
-      position: 'Computer Technician',
-      timeperiod: 'August 2016-January 2017',
-      description: 'Troubleshot, repaired, and built desktop and laptop computers'
+      company: 'Company A',
+      position: 'Developer',
+      timeperiod: 'since January 2016',
+      description: 'Programming and watching cute cat videos.'
     },
     {
-      company: 'Valley Laptop',
-      position: 'Laptop Technician/Website Administrator',
-      timeperiod: '2014-2016',
-      description: 'Refurbished laptops, offered technical support and built an ecommerce website for the company'
+      company: 'Company B',
+      position: 'Frontend Developer',
+      timeperiod: 'January 2015 - December 2015',
+      description: 'Fulfillment of extremly important tasks.'
+    },
+    {
+      company: 'Company C',
+      position: 'Trainee',
+      timeperiod: 'March 2014 - December 2014',
+      description: 'Making coffee and baking cookies.'
     }
   ],
-  projects:[{
-      name: 'Throw Remote',
-      timeperiod: 'February 2016',
-      description: 'Remote Control app for Throw streaming media player'
+  projects: [{
+    name: 'Project A',
+    timeperiod: 'March 2016',
+    description: 'Email app that holds all messages sent after 3pm on Friday and sends them to you on Monday morning. So you actually read them.' 
   },
   {
-      name: 'Emoji Rehab',
-      timeperiod: 'March 2016',
-      description: 'Community notice board for Victoria. Has the capability of marking locations with emoji.'
+    name: 'Project B',
+    timeperiod: 'February 2016',
+    description: 'News app that connects to a blood pressure monitor and adjusts your feed accordingly.'
   },
   {
-      name: 'Down2Home',
-      timeperiod: 'June 2013',
-      description: 'Saves mobile bandwidth by sending download links to a remote PC'
+    name: 'Project C',
+    timeperiod: 'June 2013',
+    description: 'App that automatically updates all your recurring credit card payments after yours inevitably gets compromised every damn year.'
   },
   {
-      name: 'Net Toolbox',
-      timeperiod: 'April 2013',
-      description: 'A suite of networking tools for professionals and enthusiasts.'
-  },
+    name: 'Project D',
+    timeperiod: 'April 2013',
+    description: 'Email app for your parents that does not come loaded with a forwarding feature.'
+  }
   ],
-  education: [
+  education: [{
+      degree: 'Master of Arts',
+      timeperiod: 'March 2012 - December 2013',
+      description: 'Major in Hacking and Computer Penetration, University A, New York, USA.'
+    },
     {
-      degree: 'Information and Computer Systems Diploma',
-      timeperiod: 'Sept 2017 - April 2019',
-      description: 'Camosun College - Victoria, BC, Canada'
+      degree: 'Bachelor of Science',
+      timeperiod: 'March 2009 - December 2011',
+      description: 'Major in Engineering, University B, Los Angeles, USA.'
     }
   ],
   // skill level goes 0 to 100
   skills: [{
-      name: 'Java',
-      level: '80'
+      name: 'HTML5',
+      level: '99'
     },
     {
-      name: 'Android SDK',
-      level: '90'
+      name: 'CSS3',
+      level: '95'
     },
     {
-      name: 'Python',
-      level: '90'
-    },
-    {
-      name: 'Go',
-      level: '75'
-    },
-    {
-      name: 'C++',
-      level: '60'
+      name: 'JavaScript',
+      level: '97'
     },
     {
       name: 'Node.js',
+      level: '93'
+    },
+    {
+      name: 'Angular 2',
+      level: '60'
+    },
+    {
+      name: 'TypeScript',
+      level: '80'
+    },
+    {
+      name: 'ES.Next',
       level: '70'
     },
     {
-      name: 'Linux',
-      level: '80'
-    },
-    {
-      name: 'AWS',
-      level: '80'
+      name: 'Docker',
+      level: '99'
     }
   ],
+  skillDescription: 'Also proficient in Adobe Photoshop and Illustrator, grew up bilingual (English and Klingon).',
   contact: {
-    email: 'paulsajna@gmail.com',
-    phone: '+1 250 891 4058',
-    street: '4402 Wilkinson Rd',
-    city: 'Victoria',
-    website: 'paulsajna.com',
-    github: 'sajattack'
+    email: 'john.doe@email.com',
+    phone: '0123 456789',
+    street: '1234 Broadway',
+    city: 'New York',
+    website: 'johndoe.com',
+    github: 'johnyD'
   }
 };

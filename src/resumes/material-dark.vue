826bc6d605d749bf90d7b2f1f0fe9a87c83bc815
Paul Sajna <paulsajna@gmail.com>
2017-09-09 23:01:25 -0700

    add projects section to material-dark

--- src/resumes/material-dark.vue
+++ src/resumes/material-dark.vue
@@ -109,15 +109,7 @@
       </div>
     </div>
 
-    <div class="section-headline">Projects</div>
-    <div class="block" v-for="project in person.projects">
-      <div class="block-helper"></div>
-      <div class="headline">{{project.name}}</h3>
-        <div class="subheadline">{{project.timeperiod}}</div>
-        <p class="info">{{project.description}}</p>
-    </div>
-    </div>
-    <div class="section-headline">Education</div>
+        <div class="section-headline">Education</div>
     <div class="block" v-for="education in person.education">
       <div class="block-helper"></div>
       <div class="headline">{{education.degree}}</div>
@@ -126,6 +118,17 @@
       </p>
     </div>
   </div>
+  <div class="farRightCol">
+    <div class="section-headline">Projects</div>
+    <div class="block" v-for="project in person.projects">
+      <div class="block-helper"></div>
+      <div class="headline">{{project.name}}</h3>
+        <div class="subheadline">{{project.timeperiod}}</div>
+        <p class="info">{{project.description}}</p>
+    </div>
+    </div>
+  </div>
+
 
   <div style="clear:both;"></div>
 </div>
@@ -271,11 +274,12 @@ h4 {
   line-height:20pt;
   opacity:1;
 }
-.rightCol {
-  width:63.5%;
+.farRightCol {
+  width:30%;
   height:100%;
   float:right;
   display:flex;
+  margin-top:120px;
   flex-direction:column;
   .block {
     width:90%;
@@ -285,7 +289,84 @@ h4 {
     margin-top:5px;
     margin-bottom:5px;
     display:inline-block;
-    box-shadow:0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
+    .headline {
+      font-weight:300;
+      display:block;
+      font-size:15px;
+      color:rgba(0,0,0,0.870588);
+    }
+    .subheadline {
+      color:rgba(0,0,0,0.541176);
+      display:block;
+      font-size:14px;
+      font-weight:300;
+    }
+    .info {
+      font-size:14px;
+      color:rgba(0,0,0,0.870588);
+      margin-bottom:0;
+      padding-top:20px;
+    }
+    .icon {
+      width:16%;
+      float:left;
+      margin-left:0;
+      .fa, .material-icons {
+        text-align:center;
+        display:block;
+        font-size:30pt;
+      }
+    }
+    .content {
+      width:80%;
+      position:absolute;
+      height:96%;
+      left:17%;
+      padding-right:3%;
+      text-align:left;
+      display:flex;
+      flex-direction:column;
+      .item {
+        border-bottom:1px solid #bdbdbd;
+        flex:1;
+        width:97%;
+        display:flex;
+        justify-content:center;
+        flex-direction:column;
+        text-align:left;
+        padding-top:0;
+        span {
+          color:#d8ab94;
+          margin-top:0;
+          font-size:10pt;
+          line-height:16pt;
+        }
+        p {
+          margin-top:5px;
+        }
+      }
+      .item:last-of-type {
+        border-bottom-style:none;
+        border-bottom-style:none;
+      }
+    }
+  }
+}
+.rightCol {
+  width:30%;
+  height:100%;
+  float:left;
+  display:flex;
+  padding-left:10px;
+  flex-direction:column;
+  .block {
+    width:90%;
+    position:relative;
+    background-color:#ffffff;
+    padding:20px;
+    margin-top:5px;
+    margin-bottom:5px;
+    display:inline-block;
     .headline {
       font-weight:300;
       display:block;

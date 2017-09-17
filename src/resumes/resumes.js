12c8bd903a1a96a57e729f65d1eaaaa2a251b68a
Matt Cheah <matt.cheah@gmail.com>
2017-09-17 03:18:46 +0000

    add purple theme with options for an about-us and job-description lists

--- src/resumes/resumes.js
+++ src/resumes/resumes.js
@@ -1,6 +1,6 @@
 // If you create a new resume, import it here:
 import '../resumes/material-dark.vue';
-import '../resumes/material-blue.vue';
 import '../resumes/left-right.vue';
 import '../resumes/oblique.vue';
 import '../resumes/side-bar.vue';
+import '../resumes/purple.vue';

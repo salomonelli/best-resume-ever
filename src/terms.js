4093afb143d32e673a085a18b9e50d2ae8964f83
Hogan Liu <hliu@archer-group.com>
2017-09-18 16:00:18 -0400

    Added Chinese language support.

--- src/terms.js
+++ src/terms.js
@@ -2,7 +2,8 @@ import en from './lang/en';
 import de from './lang/de';
 import fr from './lang/fr';
 import pt from './lang/pt';
+import cn from './lang/cn';
 
 export const terms = {
-  en, de, fr, pt
+  en, de, fr, pt, cn
 };

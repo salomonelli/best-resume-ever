912b9adf56a0f855e5eb08c9323a987ded8ac998
Mario Juarez <mario@mjp.one>
2017-09-20 01:45:43 +0200

    Add spanish language file

--- src/terms.js
+++ src/terms.js
@@ -4,7 +4,8 @@ import fr from './lang/fr';
 import pt from './lang/pt';
 import cn from './lang/cn';
 import it from './lang/it';
+import es from './lang/es';
 
 export const terms = {
-  en, de, fr, pt, cn, it
+  en, de, fr, pt, cn, it, es
 };

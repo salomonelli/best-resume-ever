9b6a550b473e104962b5faf66aaac88546d28d14
Gabriel Tibúrcio <tibuurcio@gmail.com>
2017-09-20 00:25:26 -0300

    Added support for brazilian portuguese localisation

--- src/terms.js
+++ src/terms.js
@@ -4,7 +4,8 @@ import fr from './lang/fr';
 import pt from './lang/pt';
 import cn from './lang/cn';
 import it from './lang/it';
+import ptbr from './lang/pt-br';
 
 export const terms = {
-  en, de, fr, pt, cn, it
+  en, de, fr, pt, cn, it, 'pt-br': ptbr
 };

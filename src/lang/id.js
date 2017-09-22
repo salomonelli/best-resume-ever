086e2dd7ac64ea70780153a31385aa522ebc085c
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-09-22 18:58:37 +0700

    Added indonesian language files

---
+++ src/lang/id.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const id = {
+  headings: {
+    contact: "Kontak",
+    experience: "Pengalaman",
+    education: "Pendidikan",
+    skills: "Keterampilan",
+    about: "Tentang Saya"
+  }
+};
+export default id;

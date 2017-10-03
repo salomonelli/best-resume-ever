07614f10375371eaebb2361c115fa7c2c7bff6ed
mskim <minsubkim94@gmail.com>
2017-10-03 21:58:59 +0900

    Add korean language file

---
+++ src/lang/kor.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const kor = {
+  headings: {
+    contact: "연락처",
+    experience: "경력",
+    education: "학력",
+    skills: "보유스킬",
+    about: "자기소개"
+  }
+};
+export default kor;

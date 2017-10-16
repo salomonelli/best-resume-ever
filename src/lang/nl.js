7023279c903152fe89fe0822f83e9c988c10d3d8
Mehmet Bakirci <mehmet@Mehmets-MacBook-Pro.local>
2017-10-16 15:52:11 +0200

    Added translations

---
+++ src/lang/nl.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const nl = {
+    headings: {
+      contact: 'Contact',
+      experience: 'Ervaring',
+      education: 'Opleiding',
+      skills: 'Skills',
+      about: 'Over mij'
+    }
+  };
+  export default nl;

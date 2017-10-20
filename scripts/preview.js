d23fdce562e579252b444b03cc0e0382ef391773
salomonelli <sara.steiert@outlook.com>
2017-10-20 21:57:21 +0200

    ADD catch for error

--- scripts/preview.js
+++ scripts/preview.js
@@ -3,7 +3,6 @@ const PDFImage = require('pdf-image').PDFImage;
 const path = require('path');
 const fs = require('fs');
 
-
 const getDirectories = () => {
   const srcpath = path.join(__dirname, '../pdf');
   return fs.readdirSync(srcpath);
@@ -11,7 +10,7 @@ const getDirectories = () => {
 
 const getTemplateName = PDF => {
   return PDF.replace('.pdf', '');
-}
+};
 
 const convert = async (PDF) => {
   const pdfImage = new PDFImage(path.join(__dirname, '../pdf/' + PDF));
@@ -20,11 +19,15 @@ const convert = async (PDF) => {
 
 const directories = getDirectories();
 directories.forEach(async (dir) => {
-  await convert(dir);
+  try {
+    await convert(dir);
+  } catch (e) {
+    console.dir(e);
+  }
   const source = path.join(__dirname, '../pdf/' + getTemplateName(dir) + '-0.png');
   const output = path.join(__dirname, '../src/assets/preview/resume-' + getTemplateName(dir) + '.png');
   console.log(output);
-  mv(source, output, function(err) {
-    if(err) console.dir(err);
+  mv(source, output, function (err) {
+    if (err) console.dir(err);
   });
 });

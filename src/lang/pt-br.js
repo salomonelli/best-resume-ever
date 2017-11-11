137bb1d1500b1a9cdd33e4f0a312b1aadc83b682
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2017-11-11 01:57:14 -0200

    Update the "Education" term.
    The right term that we (Brazilians) use in a CV is Formação Acadêmica, translating to English is same to say "Educational Background".

--- src/lang/pt-br.js
+++ src/lang/pt-br.js
@@ -3,7 +3,7 @@ const ptbr = {
   headings: {
     contact: 'Contato',
     experience: 'Experiência Profissional',
-    education: 'Educação',
+    education: 'Formação Acadêmica', /*You can choose, "Educação" or "Formação Acadêmica"! But the second one is more professional and is more used.*/
     skills: 'Competências',
     about: 'Sobre'
   }

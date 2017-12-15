b4e2dd59fc279768317998580353b015a9ec31b4
pubkey <daniel_van_meyer@hotmail.de>
2017-12-15 21:05:00 +0100

    UPGRADE pupeteer

--- scripts/export.js
+++ scripts/export.js
@@ -19,7 +19,7 @@ const fetchResponse = () => {
 const waitForServerReachable = () => {
   return Rx.Observable
     .interval(1000)
-    .mergeMap(async() => {
+    .mergeMap(async () => {
       try {
         const statusCode = await fetchResponse();
         if (statusCode === 200) return true;
@@ -35,17 +35,24 @@ const timedOut = timeout => {
   });
 };
 
-const convert = async() => {
+const convert = async () => {
   await waitForServerReachable().first().toPromise();
   console.log('Connected to server ...');
   console.log('Exporting ...');
   try {
     const directories = getResumesFromDirectories();
-    directories.forEach(async(dir) => {
-      const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    directories.forEach(async (dir) => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+      });
       const page = await browser.newPage();
-      await page.goto('http://localhost:8080/#/resume/' + dir.name, {waitUntil: 'networkidle', networkIdleTimeout: 5E3});
-      await page.pdf({path: path.join(__dirname, '../pdf/' + dir.name + '.pdf'), format: 'A4'});
+      await page.goto('http://localhost:8080/#/resume/' + dir.name, {
+        waitUntil: 'networkidle2'
+      });
+      await page.pdf({
+        path: path.join(__dirname, '../pdf/' + dir.name + '.pdf'),
+        format: 'A4'
+      });
       await browser.close();
     });
   } catch (err) {

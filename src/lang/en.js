9726f7c019369c9f902cb301bb7722c859f12d13
Vaibhavi Milind More <vaibmore26@gmail.com>
2019-07-22 18:42:39 -0400

    update js file with 5 additional values

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,13 +1,17 @@
 // English
 const lang = {
     contact: 'Contact',
+    reach: 'Reach Me At',
     born: 'Born',
     bornIn: 'in',
     experience: 'Experience',
     education: 'Education',
     skills: 'Skills',
+    technicalSkills: 'Technical Skills',
+    softSkills: 'Soft Skills',
     projects: 'Projects',
     contributions: 'Contributions',
-    about: 'About me'
+    about: 'About me',
+    volunteer: 'Volunteer Experience'
 };
 export default lang;

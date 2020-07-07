b83bee2f2dfb2b571c6807def18ef423aa38ac24
Edgar Zorrilla <edgar.cripter@gmail.com>
2020-07-07 03:27:19 -0500

    fixing @media print styles

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -45,21 +45,3 @@ export default Vue.component('resume', {
   overflow: hidden;
 }
 </style>
-<style media="print">
-  html,body{
-    width: 210mm;
-    height: 297mm;
-    font-size: 11px;
-    background: #FFF;
-    overflow:visible;
-  }
-  body {
-    display: table;
-    table-layout: fixed;
-  }
-  html,body,.page-wrapper,.page{padding:0;margin:0;page-break-after:always}
-  .page{width:210mm}
-  .page,.page-wrapper,.page-inner{height:297mm}
-  .section-content{padding-left:0!important}
-  body,.page-wrapper{padding:0!important}
-</style>
